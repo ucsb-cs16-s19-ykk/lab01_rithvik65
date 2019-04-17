802c3181f8f3cc15fab1ee3c9fd2f12e8dcb2bda
Rithvik Vobbilisetty <rithvik@csil-31.cs.ucsb.edu>
2019-04-16 22:09:19 -0700

    First Commit

--- block.cpp
+++ block.cpp
@@ -3,16 +3,18 @@
 using namespace std;
 
 int main(int argc, char *argv[])  {
-  int rows = 0;
-  int columns = 0;
-  cout << "Enter number of rows and columns:" << endl;
-  cin >> rows >> columns;
-  for (int r =  rows; r > 0; r--){
-      for (int c =  columns; c > 0; c--){
-	      cout <<"X."; 
+	int rows = 1;
+  	int columns = 1;
+  	while (rows != 0 and columns != 0){	
+		cout << "Enter number of rows and columns:" << endl;
+  		cin >> rows >> columns;
+  		for (int r =  rows; r > 0; r--){
+      			for (int c =  columns; c > 0; c--){
+		      		cout <<"X."; 
   }
-    cout << "\n";
-    rows--;
+    			cout << "\n";
+    			rows--;
   }
 	cout << "\n"<< endl;
-  } 
+	}
+  }
